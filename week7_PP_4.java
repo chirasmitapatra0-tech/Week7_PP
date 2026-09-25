34b25f252c973ed6859e593ee8c31aa9657623f2
chirasmitapatra0-tech <chirasmitapatra0@gmail.com>
2026-09-25 22:58:48 +0530

    Initial commit

---
+++ week7_PP_4.java
@@ -0,0 +1,25 @@
+class Locker {
+    private String code;
+    private final int lockerNumber;
+
+    Locker(int lockerNumber, String code) {
+        this.lockerNumber = lockerNumber;
+        this.code = code;
+    }
+
+    void changeCode(String oldCode, String newCode) {
+        if (code.equals(oldCode)) {
+            code = newCode;
+            System.out.println("Locker " + lockerNumber + ": Code changed successfully");
+        } else {
+            System.out.println("Locker " + lockerNumber + ": Change rejected");
+        }
+    }
+
+    public static void main(String[] args) {
+        Locker l = new Locker(101, "1234");
+
+        l.changeCode("1234", "5678");
+        l.changeCode("0000", "9999");
+    }
+}
---
+++ week7_PP_5.java
@@ -0,0 +1,45 @@
+class AttendanceSheet {
+    private String[] students;
+    private int count;
+
+    AttendanceSheet(int maxStudents) {
+        students = new String[maxStudents];
+        count = 0;
+    }
+
+    void markPresent(String name) {
+        if (isPresent(name)) {
+            return;
+        }
+
+        if (count < students.length) {
+            students[count] = name;
+            count++;
+        }
+    }
+
+    int getPresentCount() {
+        return count;
+    }
+
+    boolean isPresent(String name) {
+        for (int i = 0; i < count; i++) {
+            if (students[i].equals(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        AttendanceSheet sheet = new AttendanceSheet(30);
+
+        sheet.markPresent("Ana");
+        sheet.markPresent("Ben");
+        sheet.markPresent("Ana");
+
+        System.out.println("Present Count = " + sheet.getPresentCount());
+        System.out.println("Ben present = " + sheet.isPresent("Ben"));
+        System.out.println("Chen present = " + sheet.isPresent("Chen"));
+    }
+}
